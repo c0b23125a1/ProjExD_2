965bd1584ef3af339244f44f141ce7c008966b03
c0b23125 <c0b23125a1@edu.teu.ac.jp>
2024-10-01 17:58:43 +0900

    空白直す　＃１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -58,6 +58,7 @@ def zouka():
     
     return accs, bb_img
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -65,11 +66,11 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    # bb_image = pg.Surface((20, 20))
-    # bb_image.set_colorkey((0, 0, 0))
-    # pg.draw.circle(bb_image, (255,0,0), (10,10), 10)  # 赤、ｘ１０ｙ１０、半径１０
-    bb_image = zouka()[1]
+    bb_image = pg.Surface((20, 20))
     bb_image.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_image, (255,0,0), (10,10), 10)  # 赤、ｘ１０ｙ１０、半径１０
+    # bb_image = zouka()[1]
+    # bb_image.set_colorkey((0, 0, 0))
     bb_rct = bb_image.get_rect()
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
@@ -105,7 +106,7 @@ def main():
 
         # avx = vx*bb_accs[min(tmr//500, 9)]  # 爆弾速さ頻度
         # avy = vy*bb_accs[min(tmr//500, 9)]
-        bb_img = bb_img[min(tmr//500, 9)]
+        # bb_img = bb_img[min(tmr//500, 9)]
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:
