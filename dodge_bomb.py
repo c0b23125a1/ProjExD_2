f84f996c04a8c931fc0b3d1c959b4acd904b8b80
c0b23125 <c0b23125a1@edu.teu.ac.jp>
2024-10-01 14:52:05 +0900

    画面内判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,20 @@ DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5),
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(obj_rct: pg.Rect)  ->  tuple[bool, bool]:  # pg.Rect型だと定義するとtop,leftなどが認識される
+    """
+    引数: こうかとんrct or 爆弾rct
+    戻り値: 横方向と縦判定の真理値タプル
+    画面内:True 画面外: False
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or obj_rct.right > WIDTH:
+        yoko = False
+    if obj_rct.top < 0 or obj_rct.bottom > HEIGHT:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -47,7 +61,14 @@ def main():
                 sum_mv[0] += tpl[0]  # ヨコ方向
                 sum_mv[1] += tpl[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_image, bb_rct)
         pg.display.update()
